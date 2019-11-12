9e7fd2b419ea2968ed7c5f5c314f3f95cf14e1a7
harsha0103 <nirmalharsha0103@gmail.com>
2019-11-12 14:43:52 -0600

    Finished jump search algorithm

--- src/main/scala/Searching/binarySearchTree/jumpSearch.scala
+++ src/main/scala/Searching/binarySearchTree/jumpSearch.scala
@@ -1,9 +1,9 @@
 package searching.binarySearchTree
-
+import scala.util.control.Breaks._
 object jumpSearch {
   def main(args: Array[String]): Unit = {
-    var initailarray = Array(1, 2, 3, 4, 5, 6, 7)
-    var number =7
+    var initailarray = Array(1, 2, 3, 4, 5, 6, 7, 7, 8)
+    var number = 8
     var arraylen = initailarray.length
     var result = jumpSearchAlg(initailarray, arraylen, number)
 
@@ -17,28 +17,55 @@ object jumpSearch {
 
   def jumpSearchAlg(array: Array[Int], arraylen: Int, number: Int): Int = {
     var jumping = math.sqrt(arraylen).toInt
+    var lower = 0
+    var multfact = arraylen / jumping
     var index = -1
-    //    finding the block where the number is present
-    for (i <- 0 to arraylen - 1 by jumping) {
+    //    //    finding the block where the number is present last fucking
+    //    if (arraylen != multfact * jumping) {
+    //      if (array(arraylen - 1) >= number) {
+    //        lower = multfact * jumping
+    //        index = arraylen - 1
+    //      }
+    //    }
+    if(array(arraylen-1) >= number ){
+      index=arraylen-1
+      lower = index-jumping
+    }
 
-      if (array(i) > number) {
-        index = i
+    for (i <-  arraylen-1 to 0 by -jumping) {
+
+
+        if (array(i) >= number) {
+          index = i
+          lower = index-jumping
+
+
+        }
+
+    }
+      if (index == -1) {
+        return index
       }
-      print(i)
+    if (lower<0){
+      lower=0
     }
-    if (index == -1){
-      return index
-    }
+      for (i <- lower to index) {
+        if (array(i) == number) {
+          return i
 
-    var lower = ((index / jumping) - 1) * jumping
-
-
-    for (i <- lower to index) {
-      if (array(i) == number) {
-        return i
+        }
       }
-    }
-    return -1
-
+      return -1
   }
+
+
+  def tempIndex(array:Array[Int]):Int={
+
+    return -1
+  }
+
 }
+
+
+
+
